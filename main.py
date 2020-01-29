349d63d379745d338e0859286284e58514de921d
Stephen Thomas <sjthom39>
2020-01-29 11:17:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from tkinter import * # import Tkinter library
 window = Tk()         # Create the application window
 
 # Add a label with the text "Hello"
-lbl = Label(window, text="hi there")
-
+window.title("SCHOOL MONITOR.EXE")
+lbl = Label(window, text="Hello", font=("Courier New", 50))
 # Place the label in the window at 0, 0
+
+btn = Button(window, text="Click Me", bg="white", fg="blue")
+btn.grid(column=1, row=0)
 lbl.grid(column=0, row=0)
+def clicked():
+ 
+    lbl.configure(text="a virus has been installed")
 
 
 window.mainloop()     # Keep the window open
