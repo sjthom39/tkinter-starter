24a289be53caf0081007906aed65c6f2e9693569
Stephen Thomas <sjthom39>
2020-02-05 08:27:56 -0500

    Update story.py

--- story.py
+++ story.py
@@ -23,15 +23,16 @@ chk_state = BooleanVar()
 chk_state.set(True) #set check state
  
 chk = Checkbutton(window, text='Choose', var=chk_state)
- 
 chk.grid(column=0, row=0)
+
+combo.grid(column=0, row=0)
 combo = Combobox(window)
  
 combo['values']= (1, 2, 3, 4, 5, "secret")
  
 combo.current(1) #set the selected item
  
-combo.grid(column=0, row=0)
+
  
 
 
