c6ba7b4e317cf71155d2c6d1b52cb96e7ec48543
Stephen Thomas <sjthom39>
2020-02-06 11:16:09 -0500

    Update story.py

--- story.py
+++ story.py
@@ -6,17 +6,20 @@ window = ThemedTk(theme="elegance")
  
 window.title("smiley face")
  
-window.geometry('350x200')
+window.geometry('450x300')
 
-lbl = Label(window, text="start the story")
+lbl = Label(window, text="A strange event", font=("Arial Bold", 25))
 lbl.pack()
+
+
+
 
 def changeText():
     lbl.configure(text = "A strange event")
     # lbl['text'] = "new text"
 
-btn = Button(window, text="start botton", command=changeText)
-btn.geometry('50x20')
+btn = Button(window, text="start botton", command=nextScene)
+
 btn.pack()
 
 chk_state = BooleanVar()
@@ -33,6 +36,15 @@ combo.pack()
 combo['values']= (1, 2, 3, 4, 5, 6, 7, "secret")
  
 combo.current(0) #set the selected item
+
+
+story = Label(window, text="2077, april 20, 3:00am A strange event is happening in a power plant the dark matter genorator malfunctioned and the test subject 225 came he seemed to have the same energy radiating off him as the generator he absorbed the blast which could of destroyed the city and left other critical damages to our other power units ,the subject had memory loss he said he regains memory evryday the subjects name is jordan carter he says he comes from 2020 thats when the disaster happens when the sky goes purple weapons of mass destruction used to kill that thing in the water it seems as though it was february 7 for him. sighning off doctor.  ")
+story.pack()
+
+
+def nextScene():
+    story.configure(text = "this is where the next part of the story goes")
+
  
 
  
